ff3507fa0fee47087f3ed736e6ea2f74ca282e47
Amelia Citra <ameliacitradewi@gmail.com>
2026-01-04 19:07:29 +0700

    Update Contains Duplicate II.swift

--- Contains Duplicate II.swift
+++ Contains Duplicate II.swift
@@ -1,21 +1,28 @@
-var nums = [1,2,3,1,2,3]
-var k = 2
+/// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+var nums1 = [1,2,3,1]
+var k1 = 3
+var nums2 = [1,0,1,1]
+var k2 = 1
+var nums3 = [1,2,3,1,2,3]
+var k3 = 2
 
 class Solution {
     func containsNearbyDuplicate(_ nums: [Int], _ k: Int) -> Bool {
-        var valueToIndex = [Int: Int]()
+        var valueToIndex = [Int: Int] ()
         
-        for (index, value) in nums.enumerated() {
-            if let matchedIndex = valueToIndex[value], index - matchedIndex <= k {
+        for (currentIndex, currentValue) in nums.enumerated() {
+            if let matchedIndex = valueToIndex[currentValue], currentIndex - matchedIndex <= k {
                 return true
             }
-            valueToIndex[value] = index
+            valueToIndex[currentValue] = currentIndex
         }
-       
+        
         return false
     }
 }
 
 let solution = Solution()
-let result = solution.containsNearbyDuplicate(nums, k)
-print(result)
+print(solution.containsNearbyDuplicate(nums1, k1)) // true
+print(solution.containsNearbyDuplicate(nums2, k2)) // true
+print(solution.containsNearbyDuplicate(nums3, k3)) // false
